c55b3448a5ab67dbb8c2b4a9824fa7df008f3595
Callum <cg5290g@gre.ac.uk>
2023-11-13 10:16:35 +0000

    Updated So its only complete projects

--- Day-6 Reponsive Navbar/index.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-    var navLinks = document.getElementById("nav-link-section");
-    navLinks.classList.toggle("show");
-    
-  }
-
-  var menuIcon = document.getElementById("icon-menu");
-menuIcon.addEventListener("click", toggleNav);
